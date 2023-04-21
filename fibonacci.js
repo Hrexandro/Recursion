6fe9e361ffc91bb9a00cd5ad063a2d139e3a9c1e
Domowe <marcin_nowak_hrx@o2.pl>
2023-04-21 21:10:05 +0200

    add recursive and one line fibonacci solutions

--- fibonacci.js
+++ fibonacci.js
@@ -5,13 +5,28 @@
     // Now write another method fibsRec which solves the same problem recursively.
     //This can be done in just a couple of lines (or 1 if you’re crazy, but don’t consider either of these lengths a requirement… just get it done).
 
-    function fibs(number){
-        let result = [0]
-        for (;result.length < number;){
-            result.push( (result.length > 1 ? result[result.length-2] : 1) + result[result.length-1] )
-        }
-        return result
+function fibs (number){
+    let result = [0]
+    while (result.length < number){
+        result.push( (result.length > 1 ? result[result.length-2] : 1) + result[result.length-1] )
     }
+    return result
+}
 
-    console.log(fibs(8))
+//console.log(fibs(8))
 
+function fibsRec (number, current = [0]){
+    if (number < 2){
+        return current
+    } else {
+        let newElement = (current.length > 1 ? current[current.length-2] : 1) + current[current.length-1]
+        return fibsRec(number-1, current.concat([newElement]))
+    }
+}
+
+// console.log(fibsRec(8))
+
+function oneLinerFibsRec (number, current = [0]){
+    return (number < 2) ? current : oneLinerFibsRec(number-1, current.concat((current.length > 1 ? current[current.length-2] : 1) + current[current.length-1]))
+}
+console.log(oneLinerFibsRec(8))
